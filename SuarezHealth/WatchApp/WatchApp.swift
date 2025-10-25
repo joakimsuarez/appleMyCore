a17c9207b33220bbf18623576b9856a3400020f6
joakimsuarez <joakim@suarez.se>
2025-10-25 21:53:02 +0200

    Add SuarezHealth project with iPhoneApp and WatchApp targets

---
+++ SuarezHealth/WatchApp/WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchApp.swift
+//
+//
+
+import SwiftUI
+import appleMyCore
+
+@main
+struct WatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+//            ContentView()
+            HRVView() // från Sources/WatchApp
+        }
+    }
+}
---
+++ SuarezHealth/iPhoneApp/iPhoneApp.swift
@@ -0,0 +1,18 @@
+//
+//  iPhoneApp.swift
+//
+//
+
+import SwiftUI
+import appleMyCore // din Swift	PM-modul
+
+@main
+struct iPhoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            //ContentView()
+            HRVView() // från Sources/WatchApp
+        }
+    }
+}
+  		
